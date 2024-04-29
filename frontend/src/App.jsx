f8c42c7ebc16265ad004ae12a7213b8519bebc0b
ElizabethLankford <elizabethlankford1@gmail.com>
2024-04-29 13:59:19 -0500

    removed basic styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   }, []);
 
   const fetchContacts = async () => {
-    const response = await fetch("http://127.0.01:5000/contacts");
+    const response = await fetch("http://127.0.0.1:5000/contacts");
     const data = await response.json();
     setContacts(data.contacts);
     console.log(data.contacts);
