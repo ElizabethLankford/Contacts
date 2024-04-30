fc373fa257aa6bd01d4de84b656269220a77614c
ElizabethLankford <elizabethlankford1@gmail.com>
2024-04-30 14:32:20 -0500

    added delete functionality

--- frontend/src/ContactList.jsx
+++ frontend/src/ContactList.jsx
@@ -1,4 +1,17 @@
+/* eslint-disable react/prop-types */
+
 const ContactList = ({ contacts }) => {
+  const handleDelete = async (id) => {
+    const url = `http://127.0.0.1:5000/delete_contact/${id}`;
+    const options = {
+      method: "DELETE",
+    };
+    const response = await fetch(url, options);
+
+    const message = await response.json();
+    console.log(message);
+  };
+
   return (
     <div>
       <h2>Contacts</h2>
@@ -19,7 +32,7 @@ const ContactList = ({ contacts }) => {
               <td>{contact.email}</td>
               <td>
                 <button>Update</button>
-                <button>Delete</button>
+                <button onClick={() => handleDelete(contact.id)}>Delete</button>
               </td>
             </tr>
           ))}
